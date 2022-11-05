9c5c24e552df6ce8306b9f7f79987b4ddc179753
Ahmed Caferov <ahmetleo56@gmail.com>
2022-11-05 22:15:00 +0400

    add ESlint and Prettier

--- src/index.tsx
+++ src/index.tsx
@@ -1,58 +1,60 @@
-import { useState, useEffect, useRef } from 'react';
-import * as ReactDOM from 'react-dom';
-import * as esbuild from 'esbuild-wasm';
-import { unpkgPathPlugin } from 'plugins/unpkg-path-plugin';
+import { useState, useEffect, useRef } from "react";
+import * as ReactDOM from "react-dom";
+import * as esbuild from "esbuild-wasm";
+import { unpkgPathPlugin } from "plugins/unpkg-path-plugin";
 
 const App: React.FC = () => {
-
-  const [input,setInput] = useState('');
-  const [code,setCode] = useState('')
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
   const ref = useRef<any>();
-  
+
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: '/esbuild.wasm'
-    })
-  }
+      wasmURL: "/esbuild.wasm",
+    });
+  };
 
   useEffect(() => {
     startService();
-  },[])
+  }, []);
 
   const handleSubmit = async () => {
-    
-    if(!ref.current){
+    if (!ref.current) {
       return;
     }
-    
-    const result = await ref.current.build({ //bundling
-      entryPoints: ['index.js'],
-      bundle:true,
-      write:false,
+
+    const result = await ref.current.build({
+      //bundling
+      entryPoints: ["index.js"],
+      bundle: true,
+      write: false,
       plugins: [unpkgPathPlugin()],
       define: {
-        'process.env.NODE_ENV': '"production"',
-        global: 'window',
-      }
-    })
-    setCode(result.outputFiles[0].text) 
-  }
+        "process.env.NODE_ENV": '"production"',
+        global: "window",
+      },
+    });
+    setCode(result.outputFiles[0].text);
+  };
 
-
-  return <div>
-    <textarea value={input} onChange={(e) => {setInput(e.target.value)}}></textarea>
-    <div className="">
-      <button onClick={handleSubmit}>Submit</button>
+  return (
+    <div>
+      <textarea
+        value={input}
+        onChange={(e) => {
+          setInput(e.target.value);
+        }}
+      ></textarea>
+      <div className="">
+        <button onClick={handleSubmit}>Submit</button>
+      </div>
+      <pre>{code}</pre>
     </div>
-    <pre>{code}</pre>
-  </div>
-}
+  );
+};
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
-);
+ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
 
 // interface LocalForageDbMethodsCore {
 //   getItem<T>(
--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,61 +1,65 @@
-import * as esbuild from 'esbuild-wasm';
-import axios from 'axios';
-import localForage from 'localforage';
+import * as esbuild from "esbuild-wasm";
+import axios from "axios";
+import localForage from "localforage";
 
 const fileCache = localForage.createInstance({
-    name: 'filecache'
-})
+  name: "filecache",
+});
 export const unpkgPathPlugin = () => {
-    return {
-        name: 'unpkg-path-plugin',
-        setup(build: esbuild.PluginBuild) {
-            build.onResolve({ filter: /.*/ }, async (args: any) => {
-                if (args.path === 'index.js') {
-                    return {
-                        path: args.path, namespace: 'a'
-                    };
-                }
+  return {
+    name: "unpkg-path-plugin",
+    setup(build: esbuild.PluginBuild) {
+      build.onResolve({ filter: /.*/ }, async (args: any) => {
+        if (args.path === "index.js") {
+          return {
+            path: args.path,
+            namespace: "a",
+          };
+        }
 
-                if (args.path.includes('./') || args.path.includes('../')) {
-                    const newURL = new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/')
-                    return {
-                        namespace: 'a',
-                        path: newURL.href,
-                    }
-                }
-                return {
-                    namespace: 'a',
-                    path: `https://unpkg.com/${args.path}`,
-                }
-            });
+        if (args.path.includes("./") || args.path.includes("../")) {
+          const newURL = new URL(
+            args.path,
+            "https://unpkg.com" + args.resolveDir + "/"
+          );
+          return {
+            namespace: "a",
+            path: newURL.href,
+          };
+        }
+        return {
+          namespace: "a",
+          path: `https://unpkg.com/${args.path}`,
+        };
+      });
 
-            build.onLoad({ filter: /.*/ }, async (args: any) => {
-                if (args.path === 'index.js') {
-                    return {
-                        loader: 'jsx',
-                        contents: `import { useState } from 'nested-test-pkg'`,
-                    };
-                }
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        if (args.path === "index.js") {
+          return {
+            loader: "jsx",
+            contents: `import { useState } from 'nested-test-pkg'`,
+          };
+        }
 
-                const cachedResult = await fileCache.getItem(args.path);
+        const cachedResult = await fileCache.getItem(args.path);
 
-                if (cachedResult) {
-                    return cachedResult
-                }
+        if (cachedResult) {
+          return cachedResult;
+        }
 
-                const { data, request } = await axios.get(args.path);
-                fileCache.setItem(args.path, data)
-                const fileDir = new URL('./', request.responseURL).pathname;
+        const { data, request } = await axios.get(args.path);
+        fileCache.setItem(args.path, data);
+        const fileDir = new URL("./", request.responseURL).pathname;
 
-                const result: esbuild.OnLoadResult = {
-                    loader: 'jsx',
-                    contents: data,
-                    resolveDir: fileDir,
-                }
-                fileCache.setItem(args.path, result);
+        const result: esbuild.OnLoadResult = {
+          loader: "jsx",
+          contents: data,
+          resolveDir: fileDir,
+        };
+        fileCache.setItem(args.path, result);
 
-                return result;
-            });
-        },
-    };
+        return result;
+      });
+    },
+  };
 };
